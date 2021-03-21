a3a849ed9d19406aee78d0d71734af41bdf0f925
Nelly Bautista <nelly_bautistah@outlook.com>
2021-03-21 17:28:43 -0600

    Proyecto antes de Netlify

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Template from '@templates/Template.js';
 import './styles/main.css';
 import '@styles/vars.scss';
 
+console.log('Hola');
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
